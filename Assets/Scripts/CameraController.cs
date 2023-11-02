dad9e122a6fb32f0dd86e7c3074d50ae7189057c
tom-e666 <tptien.a1.c3tqcap@gmail.com>
2023-11-03 00:13:51 +0700

    10th

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    
-        
-    
+
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y , transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
--- Assets/Scripts/ItemsCollector.cs
+++ Assets/Scripts/ItemsCollector.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class ItemsCollecor : MonoBehaviour
+
+public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+
     [SerializeField] private Text cherriesText;
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
-        cherries++; 
-        
-        cherriesText.text = "Cherries: " + cherries;
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            if (collectionSoundEffect != null)
+            {
+                collectionSoundEffect.Play();
+            }
+            else
+            {
+                Debug.LogWarning("Collection sound effect AudioSource is not assigned on " + gameObject.name);
+            }
+
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries: " + cherries;
+        }
     }
-
-
 }
